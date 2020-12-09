32b6211cff3f7f7adc0a00ac15a0f681e1ed9a5a
jalexmelendez <62973941+jalexmelendez@users.noreply.github.com>
2020-12-09 15:16:56 -0700

    Add files via upload

---
+++ proyecto_u5.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+  int i, A=0, C=0, G=0, T=0;
+  char ADN[1000], feedBack;
+ 
+  setlocale(LC_ALL,"");
+ 
+ 
+  printf("Introduzca la cadena ADN:\n");
+     scanf("%s", &ADN[0]);
+     strupr(ADN);
+
+     for (i=0; i<1000; ++i) {
+     	if(ADN[i] == 'A'|| ADN[i] == 'C' || ADN[i] == 'G' || ADN[i] == 'T') {
+		 switch(ADN[i]){
+		 case 'A': A++; break;
+		 case 'C': C++; break;
+		 case 'G': G++; break;       
+		 case 'T': T++; break;
+		 default: break;
+		 }
+		}
+	 }
+     
+    int charLeft = 1000 - strlen(ADN);
+    
+  printf("\n{\n A:%d\n C:%d\n G:%d\n T:%d\n}\n Tu Input:[%s]\n", A,C,G,T,ADN);
+  printf("¿Desea auto generar los %d datos faltantes? [y/n]", charLeft);
+  
+  scanf("\n%c", &feedBack);
+  
+  if (feedBack == 'Y'|| feedBack == 'y') {
+  	autocomplete(charLeft,ADN);
+  }
+  
+  else {
+  	return 0;
+  }
+   
+}
+
+int autocomplete(int charLeft, char ADN[]) {
+	char pool[4] = {'A','C','G','T'};
+	int i;
+	for (i=strlen(ADN);i<charLeft;i++){
+		int dato = rand() % strlen(pool);
+		ADN[i] = pool[dato];
+	}
+	printf("\n\n GENERANDO LOS %d DATOS RESTANTES\n\n NUEVA CADENA DE ADN:\n{\n%s\n}", charLeft, ADN);
+}
+
